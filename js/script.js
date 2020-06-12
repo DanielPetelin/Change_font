87f6bf9fd0559e9063723047793e034a9920f64c
Daniil Petelin <daniilpetelin77@gmail.com>
2020-06-12 09:30:08 +0300

    add full function

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ document.querySelector("#push").onclick = () => {
       let fonts = document.querySelector("#fonts").value;
       let text = document.querySelector(".container");
 
-      if (fonts == "Arial") {
-            text.style="font-family: Arial, Helvetica, sans-serif";
+      if (fonts == "TimesNewRoman") {
+            text.style="font-family: 'Times New Roman', Times, serif";
       } else if (fonts == "Monospace") {
             text.style="font-family: monospace";
       } else {
-            text.style="font-family: 'Times New Roman', Times, serif";
+            text.style="font-family: Arial, Helvetica, sans-serif";
       }
-}
+};
